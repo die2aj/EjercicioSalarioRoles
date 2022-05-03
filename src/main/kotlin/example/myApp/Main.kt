f35e4bd3a83b3244db35b57f42fb2a448e123816
die2aj <diegoaraquej@gmail.com>
2022-05-03 12:07:46 -0500

    instance the class and add the logic

--- src/main/kotlin/example/myApp/Main.kt
+++ src/main/kotlin/example/myApp/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+package example.myapp
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+import example.myApp.Contador
+import example.myApp.Gerente
+import example.myApp.Operador
+
+
+fun crearRol(){
+    print("Que Rol desea elegir:digite 1 para Gerente,2 para Operador y 3 para Contador:  ")
+    var opcion = readLine()!!.toInt()
+    if (opcion == 1){
+        var gerente = Gerente()
+        gerente.calcularSalario()
+    }else if (opcion == 2){
+        var operador = Operador()
+        operador.calcularSalario()
+
+    }else if (opcion == 3){
+        val contador = Contador()
+        contador.calcularSalario()
+
+    }else {
+        print("Error al digitar rol")
+    }
+}
+
+
+
+
+fun main(args: Array<String>) {
+    crearRol()
 }
