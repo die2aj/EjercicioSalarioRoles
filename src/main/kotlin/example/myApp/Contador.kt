5c145eb44948a8efff88fcf5353458df1a1791bd
die2aj <diegoaraquej@gmail.com>
2022-05-03 12:04:14 -0500

    implements interface Roles and extends HorasYGananciasTrabajadas

--- src/main/kotlin/example/myApp/Contador.kt
+++ src/main/kotlin/example/myApp/Contador.kt
@@ -1,5 +1,12 @@
 package example.myApp
 
-class Contador {
+class Contador: HorasYGananciasTrabajadas(),Roles {
+    override var bonus = 0
+    override var gananciasXHora = 50
+    override var horasTrabajadas = 200
+
+    override fun calcularSalario() {
+        println("El Contador se gana en total de :" + gananciasXHora*horasTrabajadas + bonus)
+    }
 
 }
